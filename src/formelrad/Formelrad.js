d04ef8d5296cea7fcf578d600de610d24076ed9f
Berkayy19 <berkay.cinoglu1999@gmail.com>
2025-06-03 09:48:06 +0200

    If field is calculated text shall be red

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -9,7 +9,7 @@ export default function Formelrad() {
         i: 2,
         r: "",
         p: ""
-    })
+    });
 
     const [colors, setColors] = useState({
         u: "black",
@@ -17,27 +17,42 @@ export default function Formelrad() {
         r: "black",
         p: "black",
         message: "red"
-    })
+    });
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("handleSubmit")
+        console.log("handleSubmit");
+
         if (values.u === "" && values.i === "") {
-            /*calculate u and i */
+            // calculate u and i
             setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
             setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
+            setColors(colors => ({...colors, u: "red", i: "red"}));
+        } else if (values.u === "" && values.r === "") {
+            // calculate u and r
+            setValues(values => ({...values, u: values.p / values.i}));
+            setValues(values => ({...values, r: values.p / (values.i * values.i)}));
+            setColors(colors => ({...colors, u: "red", r: "red"}));
+        } else if (values.u === "" && values.p === "") {
+            // calculate u and p
+            setValues(values => ({...values, u: values.i * values.r}));
+            setValues(values => ({...values, p: values.i * values.i * values.r}));
+            setColors(colors => ({...colors, u: "red", p: "red"}));
         } else if (values.i === "" && values.r === "") {
-            /*calculate i and r */
+            // calculate i and r
             setValues(values => ({...values, i: values.p / values.u}));
-            setValues(values => ({...values, r: values.u * values.u / values.p}));
+            setValues(values => ({...values, r: (values.u * values.u) / values.p}));
+            setColors(colors => ({...colors, i: "red", r: "red"}));
         } else if (values.i === "" && values.p === "") {
-            /*calculate i and p */
+            // calculate i and p
             setValues(values => ({...values, i: values.u / values.r}));
-            setValues(values => ({...values, p: values.u * values.u / values.r}));
+            setValues(values => ({...values, p: (values.u * values.u) / values.r}));
+            setColors(colors => ({...colors, i: "red", p: "red"}));
         } else {
-            /*calculate r and p */
+            // calculate r and p
             setValues(values => ({...values, r: values.u / values.i}));
             setValues(values => ({...values, p: values.u * values.i}));
+            setColors(colors => ({...colors, r: "red", p: "red"}));
         }
     }
 
